8fd86dd676c97807f7cd03f6742b6340a0b7d636
akshayrajkore <akshayrajkore@gmail.com>
2015-12-06 03:42:41 -0500

    ARK: able to select file from listView

--- app/src/main/java/ark/com/epochanalyser/ui/MainActivity.java
+++ app/src/main/java/ark/com/epochanalyser/ui/MainActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ark.com.epochanalyser.R;
 import ark.com.epochanalyser.analyser.Analyser;
@@ -16,6 +20,8 @@ public class MainActivity extends Activity{
     private TextView mFinishTextView;
     private TextView mDurationTextView;
     private Button mStartButton;
+    private ListView mFileListView;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +33,24 @@ public class MainActivity extends Activity{
         mFinishTextView = (TextView) findViewById(R.id.textview_finish);
         mDurationTextView = (TextView) findViewById(R.id.textview_duration);
         mStartButton = (Button) findViewById(R.id.button_start);
+        mFileListView = (ListView) findViewById(R.id.list_view_file);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        mFileListView.setAdapter(new FileListAdapter(mContext));
+        mFileListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                String fileName = mFileListView.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),
+                        fileName, Toast.LENGTH_SHORT).show();
+                Analyser.setFileName(fileName);
+            }
+        });
 
         mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Analyser analyser = new Analyser((Activity) mContext, mStartTextView, mFinishTextView, mDurationTextView);
+                Analyser analyser = new Analyser((Activity) mContext, mStartTextView, mFinishTextView, mDurationTextView, mProgressBar);
+                mProgressBar.setVisibility(View.VISIBLE);
                 analyser.execute();
             }
         });
